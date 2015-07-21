b8d07c880b20137e01b9afd55aa5658d795e2f1c
lucdom <lucdom@users.noreply.github.com>
2015-07-21 16:49:21 +0200

    Removed extract method from XPathResultExtractor

--- xcrawler/core/xpath_result_extractor.py
+++ xcrawler/core/xpath_result_extractor.py
@@ -4,16 +4,6 @@ class XPathResultExtractor(object):
     """Extracts data from XPath expression result.
 
     """
-    @classmethod
-    def extract(cls, result, index = None, default_value = None, strip = False, split_pattern = None):
-        if(index != None):
-            result = cls.xpath_result_get_index(result, index, default_value)
-        if(strip == True):
-            result = cls.xpath_result_strip(result)
-        if(split_pattern != None):
-            result = cls.xpath_result_split(result, split_pattern)
-            
-        return result
 
     @classmethod
     def xpath_result_get_index(self, result, index, default_value):   
--- xcrawler/tests/core/test_xpath_result_extractor.py
+++ xcrawler/tests/core/test_xpath_result_extractor.py
@@ -8,24 +8,7 @@ class TestXPathResultExtractor(unittest.TestCase):
 
     def setUp(self):
         self.extractor = xcrawler.XPathResultExtractor
-        
-    def test_extract_no_arguments(self):
-        mock_result = ["mock_result1", "mock_result2", "mock_result3"]
-        result = self.extractor.extract(mock_result)
-        self.assertEquals(result, mock_result)   
-        
-    @mock.patch.object(xcrawler.XPathResultExtractor, 'xpath_result_get_index')
-    @mock.patch.object(xcrawler.XPathResultExtractor, 'xpath_result_strip')    
-    def test_extract_all_arguments(self, mock_extract_result_strip, mock_extract_result_get_index):
-        mock_result = ["mock_result1", "mock_result2", "mock_result3"]
-        mock_index = 1
-        mock_default_value = "Unknown"
-        mock_strip = True
-        self.extractor.extract(mock_result, mock_index, mock_default_value, mock_strip)
-        
-        mock_extract_result_get_index.assert_called_once()
-        mock_extract_result_strip.assert_called_once()
-        
+
     def test_xpath_result_get_index(self):
         mock_result = ["mock_result1", " mock_result2     ", "mock_result3;a;b;c"]
         mock_index = 1;
