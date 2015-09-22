c44fb5e61c061a308734d416e1d67cb379843921
cardsurf <cardsurf@users.noreply.github.com>
2015-09-22 12:55:30 +0200

    Renamed FirstLevelPageScraper

--- examples/stackoverflow_example_crawler.py
+++ examples/stackoverflow_example_crawler.py
@@ -1,13 +1,13 @@
 
 import xcrawler
 
-class FirstLevelPageScraper(xcrawler.PageScraper):
+class StackOverflowPageScraper(xcrawler.PageScraper):
     def extract_items(self, page):
         related_questions = page.xpath("//div[@class='module sidebar-related']//a[@class='question-hyperlink']/text()")
         return related_questions
 
 start_urls = ["http://stackoverflow.com/questions/16622802/center-image-within-div"]
-page_scrapers = [FirstLevelPageScraper()]
+page_scrapers = [StackOverflowPageScraper()]
 crawler = xcrawler.XCrawler(start_urls, page_scrapers)
 
 crawler.config.output_file_name = "stackoverflow_example_crawler_output.csv"
