8e961f745d9e57b2ebd663b3faea19d1b12790a0
cardsurf <cardsurf@users.noreply.github.com>
2015-09-28 13:25:17 +0200

    Bumped version to 1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as readme_file:
 setup(name='xcrawler',
       packages=find_packages(),
       install_requires=['lxml, cssselect'],
-      version='1.0.1',
+      version='1.1.0',
       description='A multi-threaded, open source web crawler',
       long_description=long_description,
       url='https://github.com/cardsurf/xcrawler',
