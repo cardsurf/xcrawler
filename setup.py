03ee2e81b9bad90d6faeefee108595722ab7880d
cardsurf <cardsurf@users.noreply.github.com>
2015-10-03 22:25:27 +0200

    Added six to requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as readme_file:
 
 setup(name='xcrawler',
       packages=find_packages(),
-      install_requires=['lxml', 'cssselect'],
+      install_requires=['lxml', 'cssselect', 'six'],
       version='1.1.0',
       description='A multi-threaded, open source web crawler',
       long_description=long_description,
