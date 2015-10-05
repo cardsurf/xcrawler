d0f60b3bf93eaf42f1c5d25b36cc2ba08f2b4f2b
cardsurf <cardsurf@users.noreply.github.com>
2015-10-05 15:59:55 +0200

    Added new files to __init__.py

--- xcrawler/__init__.py
+++ xcrawler/__init__.py
@@ -11,3 +11,8 @@ from xcrawler.threads.item_processor import ItemProcessor
 from xcrawler.threads.page_processor import PageProcessor
 
 from xcrawler.files.writers.item_writer import ItemWriter
+from xcrawler.files.openers.file_opener_write import FileOpenerWrite
+from xcrawler.files.strategies.objectwriting.strategy_csv_python2 import StrategyCsv
+from xcrawler.files.strategies.objectwriting.strategy_object_writing import StrategyObjectWriting
+from xcrawler.files.strategies.objectwriting.strategy_csv_python2 import StrategyCsvPython2
+from xcrawler.files.strategies.objectwriting.strategy_csv_python3 import StrategyCsvPython3
