a8ca6b9cc10e2aa9157c61b0051c439912817eb3
cardsurf <cardsurf@users.noreply.github.com>
2015-10-07 22:46:21 +0200

    Added PythonInfo

---
+++ xcrawler/tests/utils/info/test_python_info.py
@@ -0,0 +1,23 @@
+
+import unittest
+import mock
+
+from xcrawler.utils.info.python_info import PythonInfo
+
+
+class TestPythonInfo(unittest.TestCase):
+
+    def setUp(self):
+        self.python_info = PythonInfo()
+
+    @mock.patch('xcrawler.utils.info.python_info.version_info')
+    def test_is_python2(self, mock_version_info):
+        mock_version_info.major = 2
+        result = self.python_info.is_python2()
+        self.assertEquals(result, True)
+
+    @mock.patch('xcrawler.utils.info.python_info.version_info')
+    def test_is_python3(self, mock_version_info):
+        mock_version_info.major = 3
+        result = self.python_info.is_python3()
+        self.assertEquals(result, True)
---
+++ xcrawler/utils/info/python_info.py
@@ -0,0 +1,18 @@
+
+from sys import version_info
+
+
+class PythonInfo:
+    """Gets information about a running Python version.
+
+    """
+
+    def __init__(self):
+        pass
+
+    def is_python2(self):
+        return version_info.major == 2
+
+    def is_python3(self):
+        return version_info.major == 3
+
