709b5538e0956c6adbfeea9161e52a04d0175787
cardsurf <cardsurf@users.noreply.github.com>
2015-10-07 22:51:44 +0200

    Removed version_utils

--- xcrawler/compatibility/compatibility_factory.py
+++ xcrawler/compatibility/compatibility_factory.py
@@ -1,5 +1,5 @@
 
-from xcrawler.utils.version_utils import is_python2
+from xcrawler.utils.info.python_info import PythonInfo
 from xcrawler.compatibility.write_opener.write_opener_python2 import WriteOpenerPython2
 from xcrawler.compatibility.write_opener.write_opener_python3 import WriteOpenerPython3
 from xcrawler.compatibility.object_converter.object_converter_python2 import ObjectConverterPython2
@@ -8,14 +8,17 @@ from xcrawler.compatibility.object_converter.object_converter_python3 import Obj
 
 class CompatibilityFactory:
 
+    def __init__(self, python_info=PythonInfo()):
+        self.python_info = python_info
+
     def create_compatible_write_opener(self):
-        if is_python2():
+        if self.python_info.is_python2():
             return WriteOpenerPython2()
         else:
             return WriteOpenerPython3()
 
     def create_compatible_object_converter(self):
-        if is_python2():
+        if self.python_info.is_python2():
             return ObjectConverterPython2()
         else:
             return ObjectConverterPython3()
--- xcrawler/tests/compatibility/test_compatibility_factory.py
+++ xcrawler/tests/compatibility/test_compatibility_factory.py
@@ -3,46 +3,44 @@ import unittest
 import mock
 
 from xcrawler.compatibility.compatibility_factory import CompatibilityFactory
+from xcrawler.utils.info.python_info import PythonInfo
 
 
 class TestCompatibilityFactory(unittest.TestCase):
 
     def setUp(self):
-        self.factory = CompatibilityFactory()
+        python_info = mock.create_autospec(PythonInfo).return_value
+        self.factory = CompatibilityFactory(python_info)
 
-    @mock.patch('xcrawler.compatibility.compatibility_factory.is_python2')
     @mock.patch('xcrawler.compatibility.compatibility_factory.WriteOpenerPython2')
-    def test_create_file_write_strings_python2(self, mock_write_opener_python2, mock_is_python2):
+    def test_create_file_write_strings_python2(self, mock_write_opener_python2):
         mock_write_opener = mock.Mock()
         mock_write_opener_python2.return_value = mock_write_opener
-        mock_is_python2.return_value = True
+        self.factory.python_info.is_python2.return_value = True
         result = self.factory.create_compatible_write_opener()
         self.assertEquals(result, mock_write_opener)
 
-    @mock.patch('xcrawler.compatibility.compatibility_factory.is_python2')
     @mock.patch('xcrawler.compatibility.compatibility_factory.WriteOpenerPython3')
-    def test_create_file_write_strings_python3(self, mock_write_opener_python3, mock_is_python2):
+    def test_create_file_write_strings_python3(self, mock_write_opener_python3):
         mock_write_opener = mock.Mock()
         mock_write_opener_python3.return_value = mock_write_opener
-        mock_is_python2.return_value = False
+        self.factory.python_info.is_python2.return_value = False
         result = self.factory.create_compatible_write_opener()
         self.assertEquals(result, mock_write_opener)
 
-    @mock.patch('xcrawler.compatibility.compatibility_factory.is_python2')
     @mock.patch('xcrawler.compatibility.compatibility_factory.ObjectConverterPython2')
-    def test_create_object_converter_python2(self, mock_object_converter_python2, mock_is_python2):
+    def test_create_object_converter_python2(self, mock_object_converter_python2):
         mock_converter = mock.Mock()
         mock_object_converter_python2.return_value = mock_converter
-        mock_is_python2.return_value = True
+        self.factory.python_info.is_python2.return_value = True
         result = self.factory.create_compatible_object_converter()
         self.assertEquals(result, mock_converter)
 
-    @mock.patch('xcrawler.compatibility.compatibility_factory.is_python2')
     @mock.patch('xcrawler.compatibility.compatibility_factory.ObjectConverterPython3')
-    def test_create_object_converter_python3(self, mock_object_converter_python3, mock_is_python2):
+    def test_create_object_converter_python3(self, mock_object_converter_python3):
         mock_converter = mock.Mock()
         mock_object_converter_python3.return_value = mock_converter
-        mock_is_python2.return_value = False
+        self.factory.python_info.is_python2.return_value = False
         result = self.factory.create_compatible_object_converter()
         self.assertEquals(result, mock_converter)
 
