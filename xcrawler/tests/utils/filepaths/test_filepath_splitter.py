cfbff8a8c321dd767982cbab032003a63b7827d6
cardsurf <cardsurf@users.noreply.github.com>
2015-10-07 18:35:36 +0200

    Added FilePathSplitter

---
+++ xcrawler/tests/utils/filepaths/test_filepath_splitter.py
@@ -0,0 +1,18 @@
+
+import unittest
+import mock
+
+from xcrawler.utils.filepaths.filepath_splitter import FilePathSplitter
+
+
+class TestFilePathSplitter(unittest.TestCase):
+
+    def setUp(self):
+        self.filepath_splitter = FilePathSplitter()
+
+    @mock.patch('xcrawler.utils.filepaths.filepath_splitter.os.path.splitext')
+    def test_get_file_extension(self, mock_splitext_function):
+        mock_file_path = "mock/path/to/file.txt"
+        mock_splitext_function.return_value = ["file", ".txt"]
+        result = self.filepath_splitter.get_file_extension(mock_file_path)
+        self.assertEquals(result, ".txt")
---
+++ xcrawler/utils/filepaths/filepath_splitter.py
@@ -0,0 +1,16 @@
+
+import os
+
+
+class FilePathSplitter:
+    """Splits a path to a file into smaller parts.
+
+    """
+
+    def __init__(self):
+        pass
+
+    def get_file_extension(self, file_path):
+        [file_name, file_extension] = os.path.splitext(file_path)
+        return file_extension
+
