1ba115396e3a011f3f6386de544645b2ef6f829b
cardsurf <cardsurf@users.noreply.github.com>
2015-10-08 14:23:30 +0200

    Added FallbackListFactory to SelectorCss

--- xcrawler/core/selector_css.py
+++ xcrawler/core/selector_css.py
@@ -3,7 +3,8 @@ from lxml import etree
 from lxml.cssselect import CSSSelector
 
 from xcrawler.utils.converters.string_converter import StringConverter
-from xcrawler.collections.fallback_list import FallbackList
+from xcrawler.utils.factories.css_selector_factory import CSSSelectorFactory
+from xcrawler.utils.factories.fallback_list_factory import FallbackListFactory
 
 
 class SelectorCss:
@@ -12,15 +13,18 @@ class SelectorCss:
     Attributes:
         element (Element): an instance of an Element object that contains nested Elements.
         cssselector_factory (CSSSelectorFactory): creates an instance of the CSSSelector class.
+        fallbacklist_factory (FallbackListFactory): creates an instance of the FallbackList class.
         string_converter(StringConverter): the StringConverter that converts a string to an unicode string.
     """
 
     def __init__(self,
                  element=None,
-                 cssselector_factory=None,
+                 cssselector_factory=CSSSelectorFactory(),
+                 fallbacklist_factory=FallbackListFactory(),
                  string_converter=StringConverter()):
         self.element = element
         self.cssselector_factory = cssselector_factory
+        self.fallbacklist_factory = fallbacklist_factory
         self.string_converter = string_converter
 
 
--- xcrawler/tests/core/test_selector_css.py
+++ xcrawler/tests/core/test_selector_css.py
@@ -2,9 +2,10 @@
 import unittest
 import mock
 from lxml.etree import Element
-from lxml.cssselect import CSSSelector
 
 from xcrawler.core.selector_css import SelectorCss
+from xcrawler.utils.factories.css_selector_factory import CSSSelectorFactory
+from xcrawler.utils.factories.fallback_list_factory import FallbackListFactory
 from xcrawler.utils.converters.string_converter import StringConverter
 
 
@@ -12,6 +13,8 @@ class TestSelectorCss(unittest.TestCase):
 
     def setUp(self):
         element = mock.create_autospec(Element).return_value
-        selector = mock.create_autospec(CSSSelector).return_value
-        self.selector_css = SelectorCss(element, selector)
+        cssselector_factory = mock.create_autospec(CSSSelectorFactory).return_value
+        fallbacklist_factory = mock.create_autospec(FallbackListFactory).return_value
+        string_converter = mock.create_autospec(StringConverter).return_value
+        self.selector_css = SelectorCss(element, cssselector_factory, fallbacklist_factory, string_converter)
 
