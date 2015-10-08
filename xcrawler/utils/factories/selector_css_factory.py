cf34314d115093345080a00cb2da38355be7e6a4
cardsurf <cardsurf@users.noreply.github.com>
2015-10-08 13:00:56 +0200

    Added SelectorCssFactory

---
+++ xcrawler/utils/factories/selector_css_factory.py
@@ -0,0 +1,18 @@
+
+from lxml.cssselect import CSSSelector
+
+from xcrawler.core.selector_css import SelectorCss
+
+
+class SelectorCssFactory:
+    """Creates an instance of the SelectorCss class.
+
+    """
+
+    def __init__(self):
+        pass
+
+    def create_selector_css(self, element):
+        selector_css = SelectorCss(element)
+        return selector_css
+
