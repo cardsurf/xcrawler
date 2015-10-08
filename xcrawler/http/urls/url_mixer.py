0e70ae88fd516b63397c35e9c29023fc5af65437
cardsurf <cardsurf@users.noreply.github.com>
2015-10-08 22:45:28 +0200

    Use UrlInfo

--- xcrawler/http/urls/url_mixer.py
+++ xcrawler/http/urls/url_mixer.py
@@ -1,4 +1,5 @@
 
+from xcrawler.http.urls.url_info import UrlInfo
 from xcrawler.http.urls.url_joiner import UrlJoiner
 from xcrawler.http.urls.url_splitter import UrlSplitter
 
@@ -9,19 +10,26 @@ class UrlMixer:
     """
 
     def __init__(self,
+                 url_info=UrlInfo(),
                  url_joiner=UrlJoiner(),
                  url_splitter=UrlSplitter()):
+        self.url_info = url_info
         self.url_joiner = url_joiner
         self.url_splitter = url_splitter
 
-    def mix_protocol_domain_with_paths_list(self, url1, list_urls2):
+    def mix_protocol_domain_list(self, url1, list_urls2):
         urls = []
         for url2 in list_urls2:
-            url = self.mix_protocol_domain_with_path(url1, url2)
+            url = self.mix_protocol_domain(url1, url2)
             urls.append(url)
         return urls
 
-    def mix_protocol_domain_with_path(self, url1, url2):
+    def mix_protocol_domain(self, url1, url2):
+        if self.url_info.is_relative(url2):
+            url2 = self.prepend_protocol_domain(url1, url2)
+        return url2
+
+    def prepend_protocol_domain(self, url1, url2):
         protocol_domain = self.url_splitter.get_protocol_domain(url1)
         url = self.url_joiner.join_protocol_domain_to_url(protocol_domain, url2)
         return url
