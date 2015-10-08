00175d6e8d09dac5eda6fe982cedf19665ee70ac
cardsurf <cardsurf@users.noreply.github.com>
2015-10-08 14:12:03 +0200

    Added CSSSelectorFactory

---
+++ xcrawler/utils/factories/css_selector_factory.py
@@ -0,0 +1,16 @@
+
+from lxml.cssselect import CSSSelector
+
+
+class CSSSelectorFactory:
+    """Creates an instance of the CSSSelector class.
+
+    """
+
+    def __init__(self):
+        pass
+
+    def create_css_selector(self, path):
+        css_selector = CSSSelector(path)
+        return css_selector
+
