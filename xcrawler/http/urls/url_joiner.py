f642a8f26c98db71d23d8e8b348e10ea68afc548
cardsurf <cardsurf@users.noreply.github.com>
2015-10-08 22:50:06 +0200

    Shortened method names

--- xcrawler/http/urls/url_joiner.py
+++ xcrawler/http/urls/url_joiner.py
@@ -6,18 +6,16 @@ except ImportError:
 
 
 class UrlJoiner:
-    """Joins parts of an url.
+    """Joins parts of an url address to an url.
 
     """
 
-    def join_protocol_domain_to_urls(self, protocol_domain, urls):
+    def join_protocol_domain(self, protocol_domain, urls):
         for i, url in enumerate(urls):
-            urls[i] = self.join_protocol_domain_to_url(protocol_domain, url)
+            urls[i] = self.join_protocol_domain(protocol_domain, url)
         return urls
 
-    def join_protocol_domain_to_url(self, protocol_domain, url):
-        url = url
-        if not url.startswith(protocol_domain):
-            url = urljoin(protocol_domain, url)
+    def join_protocol_domain(self, protocol_domain, url):
+        url = urljoin(protocol_domain, url)
         return url
 
--- xcrawler/tests/http/test_url_joiner.py
+++ xcrawler/tests/http/test_url_joiner.py
@@ -10,21 +10,21 @@ class TestUrlJoiner(unittest.TestCase):
     def setUp(self):
         self.url_joiner = UrlJoiner()
 
-    @mock.patch.object(UrlJoiner, 'join_protocol_domain_to_url')
-    def test_join_protocol_domain_to_urls(self, mock_join_protocol_domain_to_url):
+    @mock.patch.object(UrlJoiner, 'join_protocol_domain')
+    def test_join_protocol_domain_list(self, mock_join_protocol_domain):
         mock_protocol_domain = "http://test.com"
         mock_urls = ["http://test.com/link/to/example_page.html", "link/to/example_page.html", "/link/to/example_page.html"]
-        mock_join_protocol_domain_to_url.return_value = "http://test.com/link/to/example_page.html"
-        result = self.url_joiner.join_protocol_domain_to_urls(mock_protocol_domain, mock_urls)
-        self.assertEquals(mock_join_protocol_domain_to_url.call_count, len(mock_urls))
+        mock_join_protocol_domain.return_value = "http://test.com/link/to/example_page.html"
+        result = self.url_joiner.join_protocol_domains(mock_protocol_domain, mock_urls)
+        self.assertEquals(mock_join_protocol_domain.call_count, len(mock_urls))
         self.assertEquals(result, ["http://test.com/link/to/example_page.html", "http://test.com/link/to/example_page.html",
                                    "http://test.com/link/to/example_page.html"])
 
     @mock.patch('xcrawler.http.urls.url_joiner.urljoin')
-    def test_join_protocol_domain_to_url(self, mock_urljoin_function):
+    def test_join_protocol_domain(self, mock_urljoin_function):
         mock_protocol_domain = "http://test.com"
         mock_url = ".link/to/example_page.html"
         mock_urljoin_function.return_value = ["http://test.com/link/to/example_page.html"]
-        result = self.url_joiner.join_protocol_domain_to_url(mock_protocol_domain, mock_url)
+        result = self.url_joiner.join_protocol_domain(mock_protocol_domain, mock_url)
         mock_urljoin_function.assert_called_once_with(mock_protocol_domain, mock_url)
         self.assertEquals(result, mock_urljoin_function.return_value)
