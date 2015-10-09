c77f2de850ff01ea2c36402fe6f338ef52304898
cardsurf <cardsurf@users.noreply.github.com>
2015-10-09 17:29:08 +0200

    Use ExecutorFactory

--- xcrawler/core/crawler.py
+++ xcrawler/core/crawler.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import time
 
 from xcrawler.core.config import Config
-from xcrawler.threads.work_executor import WorkExecutor
+from xcrawler.threads.executor_factory import ExecutorFactory
 
 
 class XCrawler:
@@ -14,14 +14,17 @@ class XCrawler:
         config (Config): the configuration of a crawler.
     """
     
-    def __init__(self, start_pages):
+    def __init__(self,
+                 start_pages,
+                 executor_factory=ExecutorFactory()):
         self.start_pages = start_pages
         self.config = Config()
+        self.executor_factory = executor_factory
 
     def run(self):
         start = time.time()
         if len(self.start_pages) > 0:
-            executor = WorkExecutor(self.config)
+            executor = self.executor_factory.create_work_executor(self.config)
             executor.execute_work(self.start_pages)
         end = time.time()
         print("Finished scraping. Time elapsed: " + str(end - start))
--- xcrawler/tests/core/test_crawler.py
+++ xcrawler/tests/core/test_crawler.py
@@ -4,26 +4,28 @@ import mock
 
 from xcrawler.tests.mock import mock_factory
 from xcrawler.core.crawler import XCrawler
+from xcrawler.threads.work_executor import WorkExecutor
+from xcrawler.threads.executor_factory import ExecutorFactory
 
 
 class TestXCrawler(unittest.TestCase):
     
     def setUp(self):
         start_pages = mock_factory.create_mock_pages(10)
-        self.crawler = XCrawler(start_pages)
+        executor_factory = mock.create_autospec(ExecutorFactory).return_value
+        self.crawler = XCrawler(start_pages, executor_factory)
 
-    @mock.patch('xcrawler.core.crawler.WorkExecutor')
-    def test_run_with_empty_start_pages(self, mock_work_executor_module):
-        mock_work_executor_instance = mock_work_executor_module.return_value
+    def test_run_argument_empty_start_pages(self):
+        mock_executor = mock.create_autospec(WorkExecutor).return_value
+        self.crawler.executor_factory.create_work_executor.return_value = mock_executor
         self.crawler.start_pages = []
         self.crawler.run()
-        self.assertFalse(mock_work_executor_instance.execute_work.called)
+        self.assertFalse(mock_executor.execute_work.called)
 
-    @mock.patch('xcrawler.core.crawler.WorkExecutor')
-    def test_run_with_non_empty_start_pages(self, mock_work_executor_module):
-        mock_work_executor_instance = mock_work_executor_module.return_value
-        self.crawler.start_pages = mock_factory.create_mock_pages(10)
+    def test_run_argument_non_empty_start_pages(self):
+        mock_executor = mock.create_autospec(WorkExecutor).return_value
+        self.crawler.executor_factory.create_work_executor.return_value = mock_executor
         self.crawler.run()
-        mock_work_executor_instance.execute_work.assert_called_once_with(self.crawler.start_pages)
+        mock_executor.execute_work.assert_called_once_with(self.crawler.start_pages)
 
 
