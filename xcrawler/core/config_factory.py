a270131a0cfaa1e175d06a5d48541554bfa0cd42
cardsurf <cardsurf@users.noreply.github.com>
2015-10-09 17:32:48 +0200

    Created ConfigFactory

---
+++ xcrawler/core/config_factory.py
@@ -0,0 +1,14 @@
+
+from xcrawler.core.config import Config
+
+
+class ConfigFactory:
+    """Creates a configuration of a crawler.
+
+    """
+    def __init__(self):
+        pass
+
+    def create_config(self):
+        config = Config()
+        return config
---
+++ xcrawler/tests/core/test_config_factory.py
@@ -0,0 +1,19 @@
+
+import unittest
+import mock
+
+from xcrawler.core.config_factory import ConfigFactory
+from xcrawler.core.config import Config
+
+
+class TestConfigFactory(unittest.TestCase):
+
+    def setUp(self):
+        self.config_factory = ConfigFactory()
+
+    @mock.patch('xcrawler.core.config_factory.Config')
+    def test_create_queue(self, mock_config_class):
+        mock_config = mock.create_autospec(Config).return_value
+        mock_config_class.return_value = mock_config
+        result = self.config_factory.create_config()
+        self.assertEquals(result, mock_config)
