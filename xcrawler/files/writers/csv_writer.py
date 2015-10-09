ab10f83f99b9f145c9f5572cbbb75f96ebd83c3a
cardsurf <cardsurf@users.noreply.github.com>
2015-10-10 00:28:29 +0200

    Added default arguments to csv.writer

--- xcrawler/files/writers/csv_writer.py
+++ xcrawler/files/writers/csv_writer.py
@@ -8,6 +8,10 @@ class CsvWriterFactory:
     def __init__(self):
         pass
 
-    def create_csv_writer(self, opened_file):
-        csv_writer = csv.writer(opened_file)
+    def create_csv_writer(self, opened_file, delimiter=' ', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n'):
+        csv_writer = csv.writer(opened_file,
+                                delimiter=delimiter,
+                                quotechar=quotechar,
+                                quoting=quoting,
+                                lineterminator=lineterminator)
         return csv_writer
