999affefc496931c6cea1c49188a1cdca6b319c2
cardsurf <cardsurf@users.noreply.github.com>
2015-10-09 18:39:19 +0200

    Created queues directory

--- xcrawler/tests/threads/test_queue_factory.py
+++ xcrawler/tests/threads/test_queue_factory.py
@@ -1,12 +1,13 @@
-
 import unittest
+
 import mock
+
 try:
     import Queue as queue
 except ImportError:
     import queue
 
-from xcrawler.threads.queue_factory import QueueFactory
+from xcrawler.threads.queues.queue_factory import QueueFactory
 
 
 class TestQueueFactory(unittest.TestCase):
@@ -14,7 +15,7 @@ class TestQueueFactory(unittest.TestCase):
     def setUp(self):
         self.queue_factory = QueueFactory()
 
-    @mock.patch('xcrawler.threads.queue_factory.queue.Queue')
+    @mock.patch('xcrawler.threads.queues.queue_factory.queue.Queue')
     def test_create_queue(self, mock_queue_class):
         mock_queue = mock.create_autospec(queue.Queue).return_value
         mock_queue_class.return_value = mock_queue
