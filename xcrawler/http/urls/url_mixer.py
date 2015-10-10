0d25678eef8f6a4b12a65a1e558c2b61d0fcdccf
cardsurf <cardsurf@users.noreply.github.com>
2015-10-10 15:27:51 +0200

    Use urljoin in UrlMixer

--- xcrawler/http/urls/url_mixer.py
+++ xcrawler/http/urls/url_mixer.py
@@ -1,28 +1,30 @@
 
+try:
+    from urlparse import urljoin
+except ImportError:
+    from urllib.parse import urljoin
+
 from xcrawler.http.urls.url_info import UrlInfo
-from xcrawler.http.urls.url_joiner import UrlJoiner
 from xcrawler.http.urls.url_splitter import UrlSplitter
 
 
 class UrlMixer:
-    """Join parts of the first url to the the second url.
+    """Replaces missing parts of the second url with the parts of the first url.
 
     """
 
     def __init__(self,
                  url_info=UrlInfo(),
-                 url_joiner=UrlJoiner(),
                  url_splitter=UrlSplitter()):
         self.url_info = url_info
-        self.url_joiner = url_joiner
         self.url_splitter = url_splitter
 
     def mix_protocol_domain(self, url1, url2):
         if self.url_info.is_relative(url2):
-            url2 = self.prepend_protocol_domain(url1, url2)
+            protocol_domain = self.url_splitter.get_protocol_domain(url1)
+            url2 = self.add_protocol_domain(protocol_domain, url2)
         return url2
 
-    def prepend_protocol_domain(self, url1, url2):
-        protocol_domain = self.url_splitter.get_protocol_domain(url1)
-        url = self.url_joiner.join_protocol_domain(protocol_domain, url2)
+    def add_protocol_domain(self, protocol_domain, url):
+        url = urljoin(protocol_domain, url)
         return url
--- xcrawler/tests/http/urls/test_url_mixer.py
+++ xcrawler/tests/http/urls/test_url_mixer.py
@@ -4,7 +4,6 @@ import mock
 
 from xcrawler.http.urls.url_mixer import UrlMixer
 from xcrawler.http.urls.url_info import UrlInfo
-from xcrawler.http.urls.url_joiner import UrlJoiner
 from xcrawler.http.urls.url_splitter import UrlSplitter
 
 
@@ -12,23 +11,25 @@ class TestUrlMixer(unittest.TestCase):
 
     def setUp(self):
         url_info = mock.create_autospec(UrlInfo).return_value
-        url_joiner = mock.create_autospec(UrlJoiner).return_value
         url_splitter = mock.create_autospec(UrlSplitter).return_value
-        self.url_mixer = UrlMixer(url_info, url_joiner, url_splitter)
+        self.url_mixer = UrlMixer(url_info, url_splitter)
 
-    @mock.patch.object(UrlMixer, 'prepend_protocol_domain')
-    def test_mix_protocol_domains(self, mock_prepend_protocol_domain):
+    @mock.patch.object(UrlMixer, 'add_protocol_domain')
+    def test_mix_protocol_domain(self, mock_add_protocol_domain):
         mock_url1 = "http://example.com/path/to/mock_url1.html"
+        mock_protocol_domain = "http://example.com"
         mock_url2 = "link/to/example_page.html"
         self.url_mixer.url_info.is_relative.return_value = True
-        mock_prepend_protocol_domain.return_value = "http://example.com/link/to/example_page.html"
+        self.url_mixer.url_splitter.get_protocol_domain.return_value = mock_protocol_domain
+        mock_add_protocol_domain.return_value = "http://example.com/link/to/example_page.html"
         result = self.url_mixer.mix_protocol_domain(mock_url1, mock_url2)
         self.assertEquals(result, "http://example.com/link/to/example_page.html")
 
-    def test_prepend_protocol_domain(self):
-        mock_url1 = "http://example.com/path/to/mock_url1.html"
-        mock_url2 = "link/to/example_page.html"
-        self.url_mixer.url_splitter.get_protocol_domain.return_value = "http://example.com/"
-        self.url_mixer.url_joiner.join_protocol_domain.return_value = "http://example.com/link/to/example_page.html"
-        result = self.url_mixer.mix_protocol_domain(mock_url1, mock_url2)
-        self.assertEquals(result, "http://example.com/link/to/example_page.html")
+    @mock.patch('xcrawler.http.urls.url_mixer.urljoin')
+    def test_add_protocol_domain(self, mock_urljoin_function):
+        mock_protocol_domain = "http://test.com"
+        mock_url = ".link/to/example_page.html"
+        mock_urljoin_function.return_value = ["http://test.com/link/to/example_page.html"]
+        result = self.url_mixer.add_protocol_domain(mock_protocol_domain, mock_url)
+        mock_urljoin_function.assert_called_once_with(mock_protocol_domain, mock_url)
+        self.assertEquals(result, mock_urljoin_function.return_value)
