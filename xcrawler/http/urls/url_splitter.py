1156682905ae0f75c37fb8be4d7e620dc20379b5
cardsurf <cardsurf@users.noreply.github.com>
2015-10-10 23:26:50 +0200

    Added format_to_parsable_url

--- xcrawler/http/urls/url_splitter.py
+++ xcrawler/http/urls/url_splitter.py
@@ -1,4 +1,5 @@
 
+import re
 try:
     from urlparse import urlparse
 except ImportError:
@@ -9,9 +10,31 @@ class UrlSplitter:
     """Splits an url into smaller parts.
 
     """
+    def __init__(self):
+        self.protocol_domain_separator = "//"
+        self.path_separator = "/"
+
     def parse_url(self, url):
+        url = self.format_to_parsable_url(url)
         parsed_url = urlparse(url)
         return parsed_url
+
+    def format_to_parsable_url(self, url):
+        if not self.protocol_domain_separator in url:
+            url = self.add_protocol_domain_separator(url)
+        return url
+
+    def add_protocol_domain_separator(self, url):
+        url = url.lstrip(self.path_separator)
+        first_part_url = url.split(self.path_separator)[0]
+        if self.is_domain(first_part_url):
+            url = self.protocol_domain_separator + url
+        return url
+
+    def is_domain(self, string):
+        is_domain_regex = "^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}$"
+        match = re.match(is_domain_regex, string)
+        return match is not None
 
     def get_part_url(self, string_pattern, parsed_url):
         part = string_pattern.format(uri=parsed_url)
--- xcrawler/tests/http/urls/test_url_splitter.py
+++ xcrawler/tests/http/urls/test_url_splitter.py
@@ -15,15 +15,47 @@ class TestUrlSplitter(unittest.TestCase):
         self.url_splitter = UrlSplitter()
 
     @mock.patch('xcrawler.http.urls.url_splitter.urlparse')
-    def test_parse_url(self, mock_urlparse_function):
-        mock_url = "http://test.com/link/to/example_page.html"
+    @mock.patch.object(UrlSplitter, 'format_to_parsable_url')
+    def test_parse_url(self, mock_format_to_parsable_url, mock_urlparse_function):
+        mock_url = "test.com/link/to/example_page.html"
+        mock_formatted_url = "//test.com/link/to/example_page.html"
+        mock_format_to_parsable_url.return_value = mock_formatted_url
         mock_parsed_url = mock.create_autospec(ParseResult).return_value
         mock_urlparse_function.return_value = mock_parsed_url
         result = self.url_splitter.parse_url(mock_url)
-        mock_urlparse_function.assert_called_once_with(mock_url)
+        mock_urlparse_function.assert_called_once_with("//test.com/link/to/example_page.html")
         self.assertEquals(result, mock_parsed_url)
 
-    def test_part_url(self):
+    @mock.patch.object(UrlSplitter, 'add_protocol_domain_separator')
+    def test_format_to_parsable_url(self, mock_add_protocol_domain_separator):
+        mock_url = "test.com/link/to/example_page.html"
+        mock_add_protocol_domain_separator.return_value = "//test.com/link/to/example_page.html"
+        result = self.url_splitter.format_to_parsable_url(mock_url)
+        self.assertEquals(result, "//test.com/link/to/example_page.html")
+
+    @mock.patch.object(UrlSplitter, 'is_domain')
+    def test_add_protocol_domain_separator(self, mock_is_domain):
+        mock_url = "test.com/link/to/example_page.html"
+        mock_is_domain.return_value = True
+        result = self.url_splitter.add_protocol_domain_separator(mock_url)
+        self.assertEquals(result, "//test.com/link/to/example_page.html")
+
+    @mock.patch('xcrawler.http.urls.url_splitter.re.match')
+    def test_is_domain_argument_valid_domain(self, mock_match_function):
+        mock_domain = "test.com"
+        mock_sre_match = mock.Mock()
+        mock_match_function.return_value = mock_sre_match
+        result = self.url_splitter.is_domain(mock_domain)
+        self.assertEquals(result, True)
+
+    @mock.patch('xcrawler.http.urls.url_splitter.re.match')
+    def test_is_domain_argument_invalid_domain(self, mock_match_function):
+        mock_domain = "test,com"
+        mock_match_function.return_value = None
+        result = self.url_splitter.is_domain(mock_domain)
+        self.assertEquals(result, False)
+
+    def test_get_part_url(self):
         mock_parsed_url = mock.create_autospec(ParseResult).return_value
         mock_parsed_url.__str__ = "http://test.com/link/to/example_page.html"
         mock_pattern = "{uri.netloc}"
