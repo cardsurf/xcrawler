3360a7ed78a4ca5c1b6d96693941354fa40a8ac5
cardsurf <cardsurf@users.noreply.github.com>
2015-10-11 16:32:56 +0200

    Removed url_validator

--- tests/http/urls/test_url_splitter.py
+++ tests/http/urls/test_url_splitter.py
@@ -7,39 +7,21 @@ except ImportError:
     from urllib.parse import ParseResult
 
 from xcrawler.http.urls.url_splitter import UrlSplitter
-from xcrawler.http.urls.url_validator import UrlValidator
 
 
 class TestUrlSplitter(unittest.TestCase):
 
     def setUp(self):
-        url_validator = mock.create_autospec(UrlValidator).return_value
-        self.url_splitter = UrlSplitter(url_validator)
+        self.url_splitter = UrlSplitter()
 
     @mock.patch('xcrawler.http.urls.url_splitter.urlparse')
-    @mock.patch.object(UrlSplitter, 'format_to_parsable_url')
-    def test_parse_url(self, mock_format_to_parsable_url, mock_urlparse_function):
-        mock_url = "test.com/link/to/example_page.html"
-        mock_formatted_url = "//test.com/link/to/example_page.html"
-        mock_format_to_parsable_url.return_value = mock_formatted_url
+    def test_parse_url(self, mock_urlparse_function):
+        mock_url = "http://test.com/link/to/example_page.html"
         mock_parsed_url = mock.create_autospec(ParseResult).return_value
         mock_urlparse_function.return_value = mock_parsed_url
         result = self.url_splitter.parse_url(mock_url)
-        mock_urlparse_function.assert_called_once_with("//test.com/link/to/example_page.html")
+        mock_urlparse_function.assert_called_once_with("http://test.com/link/to/example_page.html")
         self.assertEquals(result, mock_parsed_url)
-
-    @mock.patch.object(UrlSplitter, 'add_protocol_domain_separator')
-    def test_format_to_parsable_url(self, mock_add_protocol_domain_separator):
-        mock_url = "test.com/link/to/example_page.html"
-        mock_add_protocol_domain_separator.return_value = "//test.com/link/to/example_page.html"
-        result = self.url_splitter.format_to_parsable_url(mock_url)
-        self.assertEquals(result, "//test.com/link/to/example_page.html")
-
-    def test_add_protocol_domain_separator(self):
-        mock_url = "test.com/link/to/example_page.html"
-        self.url_splitter.url_validator.is_domain.return_value = True
-        result = self.url_splitter.add_protocol_domain_separator(mock_url)
-        self.assertEquals(result, "//test.com/link/to/example_page.html")
 
     def test_get_part_url(self):
         mock_parsed_url = mock.create_autospec(ParseResult).return_value
--- xcrawler/http/urls/url_splitter.py
+++ xcrawler/http/urls/url_splitter.py
@@ -11,28 +11,12 @@ class UrlSplitter:
     """Splits an url into smaller parts.
 
     """
-    def __init__(self,
-                 url_validator=UrlValidator()):
-        self.url_validator = url_validator
-        self.protocol_domain_separator = "//"
-        self.path_separator = "/"
+    def __init__(self):
+        pass
 
     def parse_url(self, url):
-        url = self.format_to_parsable_url(url)
         parsed_url = urlparse(url)
         return parsed_url
-
-    def format_to_parsable_url(self, url):
-        if not self.protocol_domain_separator in url:
-            url = self.add_protocol_domain_separator(url)
-        return url
-
-    def add_protocol_domain_separator(self, url):
-        url = url.lstrip(self.path_separator)
-        first_part_url = url.split(self.path_separator)[0]
-        if self.url_validator.is_domain(first_part_url):
-            url = self.protocol_domain_separator + url
-        return url
 
     def get_part_url(self, string_pattern, parsed_url):
         part = string_pattern.format(uri=parsed_url)
