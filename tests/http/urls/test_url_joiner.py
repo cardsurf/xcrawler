23353bd26b680198e9167cfa39daac0b8576d732
cardsurf <cardsurf@users.noreply.github.com>
2015-10-11 16:30:39 +0200

    Format urls before join

--- tests/http/urls/test_url_joiner.py
+++ tests/http/urls/test_url_joiner.py
@@ -4,19 +4,23 @@ import mock
 
 from xcrawler.http.urls.url_joiner import UrlJoiner
 from xcrawler.http.urls.url_splitter import UrlSplitter
+from xcrawler.http.urls.url_formatter import UrlFormatter
 
 
 class TestUrlJoiner(unittest.TestCase):
 
     def setUp(self):
+        url_formatter = mock.create_autospec(UrlFormatter).return_value
         url_splitter = mock.create_autospec(UrlSplitter).return_value
-        self.url_joiner = UrlJoiner(url_splitter)
+        self.url_joiner = UrlJoiner(url_formatter, url_splitter)
 
     @mock.patch.object(UrlJoiner, 'add_protocol_domain')
-    def test_join_protocol_domain(self, mock_add_protocol_domain):
+    @mock.patch.object(UrlJoiner, 'format_urls')
+    def test_join_protocol_domain(self, mock_format_urls, mock_add_protocol_domain):
         mock_url1 = "http://example.com/path/to/mock_url1.html"
         mock_protocol_domain = "http://example.com"
         mock_url2 = "link/to/example_page.html"
+        mock_format_urls.return_value = ["http://example.com/path/to/mock_url1.html", "//link/to/example_page.html"]
         self.url_joiner.url_splitter.get_protocol_domain.return_value = mock_protocol_domain
         mock_add_protocol_domain.return_value = "http://example.com/link/to/example_page.html"
         result = self.url_joiner.join_protocol_domain(mock_url1, mock_url2)
@@ -30,3 +34,11 @@ class TestUrlJoiner(unittest.TestCase):
         result = self.url_joiner.add_protocol_domain(mock_protocol_domain, mock_url)
         mock_urljoin_function.assert_called_once_with(mock_protocol_domain, mock_url)
         self.assertEquals(result, mock_urljoin_function.return_value)
+
+    def test_format_urls(self):
+        mock_url1 = "/test.com/link/to/example_page.html"
+        mock_url2 = "test.com/link/to/example_page.html"
+        self.url_joiner.url_formatter.format_to_parsable_url.return_value = "//test.com/link/to/example_page.html"
+        result = self.url_joiner.format_urls(mock_url1, mock_url2)
+        self.assertEquals(result, ["//test.com/link/to/example_page.html", "//test.com/link/to/example_page.html"])
+
--- xcrawler/http/urls/url_joiner.py
+++ xcrawler/http/urls/url_joiner.py
@@ -4,6 +4,7 @@ try:
 except ImportError:
     from urllib.parse import urljoin
 
+from xcrawler.http.urls.url_formatter import UrlFormatter
 from xcrawler.http.urls.url_splitter import UrlSplitter
 
 
@@ -13,13 +14,21 @@ class UrlJoiner:
     """
 
     def __init__(self,
+                 url_formatter=UrlFormatter(),
                  url_splitter=UrlSplitter()):
+        self.url_formatter = url_formatter
         self.url_splitter = url_splitter
 
     def join_protocol_domain(self, url1, url2):
+        url1, url2 = self.format_urls(url1, url2)
         protocol_domain = self.url_splitter.get_protocol_domain(url1)
         url2 = self.add_protocol_domain(protocol_domain, url2)
         return url2
+
+    def format_urls(self, url1, url2):
+        url1 = self.url_formatter.format_to_parsable_url(url1)
+        url2 = self.url_formatter.format_to_parsable_url(url2)
+        return [url1, url2]
 
     def add_protocol_domain(self, protocol_domain, url):
         url = urljoin(protocol_domain, url)
