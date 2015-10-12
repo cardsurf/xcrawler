8e7f1d5d5559a51f0fd64ed5f0dbe90c1042e1a3
cardsurf <cardsurf@users.noreply.github.com>
2015-10-12 14:14:14 +0200

    Added a compatibility_factory attribute

--- tests/files/writers/test_object_writer_factory.py
+++ tests/files/writers/test_object_writer_factory.py
@@ -3,28 +3,28 @@ import unittest
 import mock
 
 from xcrawler.compatibility.compatibility_factory import CompatibilityFactory
+from xcrawler.compatibility.write_opener.compatible_write_opener import CompatibleWriteOpener
+from xcrawler.compatibility.object_converter.compatible_object_converter import CompatibleObjectConverter
+from xcrawler.files.writers.object_writer import ObjectWriter
 from xcrawler.files.writers.object_writer_factory import ObjectWriterFactory
 
 
 class TestObjectWriterFactory(unittest.TestCase):
 
     def setUp(self):
-        self.factory = ObjectWriterFactory()
+        compatibility_factory = mock.create_autospec(CompatibilityFactory).return_value
+        self.object_writer_factory = ObjectWriterFactory(compatibility_factory)
 
-    @mock.patch('xcrawler.files.writers.object_writer_factory.CompatibilityFactory')
     @mock.patch('xcrawler.files.writers.object_writer_factory.ObjectWriterCsv')
-    def test_create_object_writer_csv(self, mock_object_writer_csv_class, mock_compatibility_factory_class):
-        mock_factory_instance = mock.create_autospec(CompatibilityFactory).return_value
-        mock_file_opener = mock.Mock()
-        mock_object_converter = mock.Mock()
-        mock_object_writer = mock.Mock()
-
-        mock_compatibility_factory_class.return_value = mock_factory_instance
-        mock_factory_instance.create_compatible_write_opener.return_value = mock_file_opener
-        mock_factory_instance.create_compatible_object_converter.return_value = mock_object_converter
+    def test_create_object_writer_csv(self, mock_object_writer_csv_class):
+        mock_write_opener = mock.create_autospec(CompatibleWriteOpener).return_value
+        mock_object_converter = mock.create_autospec(CompatibleObjectConverter).return_value
+        mock_object_writer = mock.create_autospec(ObjectWriter).return_value
+        self.object_writer_factory.compatibility_factory.create_compatible_write_opener.return_value = mock_write_opener
+        self.object_writer_factory.compatibility_factory.create_compatible_object_converter.return_value = mock_object_converter
         mock_object_writer_csv_class.return_value = mock_object_writer
-        result = self.factory.create_object_writer_csv()
-        mock_object_writer_csv_class.assert_called_once_with(mock_file_opener, mock_object_converter)
+        result = self.object_writer_factory.create_object_writer_csv()
+        mock_object_writer_csv_class.assert_called_once_with(mock_write_opener, mock_object_converter)
         self.assertEquals(result, mock_object_writer)
 
 
--- xcrawler/files/writers/object_writer_factory.py
+++ xcrawler/files/writers/object_writer_factory.py
@@ -5,13 +5,13 @@ from xcrawler.files.writers.object_writer_csv import ObjectWriterCsv
 
 class ObjectWriterFactory(object):
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 compatibility_factory=CompatibilityFactory()):
+        self.compatibility_factory = compatibility_factory
 
     def create_object_writer_csv(self):
-        factory = CompatibilityFactory()
-        file_opener = factory.create_compatible_write_opener()
-        object_converter = factory.create_compatible_object_converter()
+        file_opener = self.compatibility_factory.create_compatible_write_opener()
+        object_converter = self.compatibility_factory.create_compatible_object_converter()
         object_writer_csv = ObjectWriterCsv(file_opener, object_converter)
         return object_writer_csv
 
