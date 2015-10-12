cc45e135fd0b75e906a3b7ef3f7d03b44a0a576a
cardsurf <cardsurf@users.noreply.github.com>
2015-10-12 17:24:53 +0200

    Removed thread and file classes

--- xcrawler/__init__.py
+++ xcrawler/__init__.py
@@ -1,16 +1,7 @@
 
 from xcrawler.collections.fallback_list import FallbackList
-
 from xcrawler.core.crawler.crawler import XCrawler
 from xcrawler.core.crawler.page_scraper import PageScraper
 from xcrawler.core.crawler.config import Config
 from xcrawler.core.crawler.page import Page
 
-from xcrawler.threads.work_executor import WorkExecutor
-from xcrawler.threads.item_processor import ItemProcessor
-from xcrawler.threads.page_processor import PageProcessor
-
-from xcrawler.files.writers.item_writer import ItemWriter
-from xcrawler.files.openers.write_opener import WriteOpener
-from xcrawler.files.writers.object_writer_csv import ObjectWriterCsv
-from xcrawler.files.writers.object_writer import ObjectWriter
