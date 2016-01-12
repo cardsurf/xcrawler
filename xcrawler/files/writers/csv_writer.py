04dedb98c720b15e93b7f2a1e15edf3f566de6d9
cardsurf <cardsurf@mail.com>
2016-01-12 11:20:03 +0100

    Set default csv quoting to QUOTE_ALL

--- xcrawler/files/writers/csv_writer.py
+++ xcrawler/files/writers/csv_writer.py
@@ -8,7 +8,7 @@ class CsvWriterFactory(object):
     def __init__(self):
         pass
 
-    def create_csv_writer(self, opened_file, delimiter=' ', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n'):
+    def create_csv_writer(self, opened_file, delimiter=' ', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n'):
         csv_writer = csv.writer(opened_file,
                                 delimiter=delimiter,
                                 quotechar=quotechar,
