9189ef5015d099450b6b624f4f9e42a453a48537
cardsurf <cardsurf@mail.com>
2016-01-17 10:26:00 +0100

    Set minimum version requests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as readme_file:
 
 setup(name='xcrawler',
       packages=find_packages(),
-      install_requires=['lxml', 'cssselect', 'six', 'requests>=0.13'],
+      install_requires=['lxml', 'cssselect', 'six', 'requests>=2.4.0'],
       version='1.2.0',
       description='A multi-threaded, open source web crawler',
       long_description=long_description,
