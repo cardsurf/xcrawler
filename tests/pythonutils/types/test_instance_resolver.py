9c1dd9a06222125be7e9406e25bfa8048f126f4d
cardsurf <cardsurf@mail.com>
2016-01-18 10:55:30 +0100

    Added is_unicode_or_byte_string

--- tests/pythonutils/types/test_instance_resolver.py
+++ tests/pythonutils/types/test_instance_resolver.py
@@ -10,24 +10,14 @@ class TestInstanceResolver(unittest.TestCase):
     def setUp(self):
         self.type_resolver = InstanceResolver()
 
-    def test_is_string_python_version_argument_non_string(self):
+    def test_is_unicode_or_byte_string_argument_non_string(self):
         mock_object = mock.Mock()
-        result = self.type_resolver.is_string_python_version(mock_object)
+        result = self.type_resolver.is_unicode_or_byte_string(mock_object)
         self.assertEquals(result, False)
 
-    def test_is_string_python_version_argument_string(self):
-        mock_object = "mock"
-        result = self.type_resolver.is_string_python_version(mock_object)
-        self.assertEquals(result, True)
-
-    def test_is_byte_string_argument_non_byte_string(self):
-        mock_object = mock.Mock()
-        result = self.type_resolver.is_byte_string(mock_object)
-        self.assertEquals(result, False)
-
-    def test_is_byte_string_argument_byte_string(self):
+    def test_is_unicode_or_byte_string_argument_string(self):
         mock_object = b"mock"
-        result = self.type_resolver.is_byte_string(mock_object)
+        result = self.type_resolver.is_unicode_or_byte_string(mock_object)
         self.assertEquals(result, True)
 
     def test_is_unicode_string_argument_non_unicode_string(self):
@@ -40,4 +30,23 @@ class TestInstanceResolver(unittest.TestCase):
         result = self.type_resolver.is_unicode_string(mock_object)
         self.assertEquals(result, True)
 
+    def test_is_byte_string_argument_non_byte_string(self):
+        mock_object = mock.Mock()
+        result = self.type_resolver.is_byte_string(mock_object)
+        self.assertEquals(result, False)
+
+    def test_is_byte_string_argument_byte_string(self):
+        mock_object = b"mock"
+        result = self.type_resolver.is_byte_string(mock_object)
+        self.assertEquals(result, True)
+
+    def test_is_string_python_version_argument_non_string(self):
+        mock_object = mock.Mock()
+        result = self.type_resolver.is_string_python_version(mock_object)
+        self.assertEquals(result, False)
+
+    def test_is_string_python_version_argument_string(self):
+        mock_object = "mock"
+        result = self.type_resolver.is_string_python_version(mock_object)
+        self.assertEquals(result, True)
 
--- xcrawler/pythonutils/types/instance_resolver.py
+++ xcrawler/pythonutils/types/instance_resolver.py
@@ -6,12 +6,15 @@ class InstanceResolver(object):
     """Determines if an object is an instance of a specified type.
 
     """
-    def is_string_python_version(self, o):
-        return isinstance(o, string_types)
+    def is_unicode_or_byte_string(self, o):
+        return self.is_unicode_string(o) or self.is_byte_string(o)
+
+    def is_unicode_string(self, o):
+        return isinstance(o, text_type)
 
     def is_byte_string(self, o):
         return isinstance(o, binary_type)
 
-    def is_unicode_string(self, o):
-        return isinstance(o, text_type)
+    def is_string_python_version(self, o):
+        return isinstance(o, string_types)
 
